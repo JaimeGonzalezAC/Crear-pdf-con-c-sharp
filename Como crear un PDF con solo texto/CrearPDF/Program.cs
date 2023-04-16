49ed6c0c32dda4be4f60ab94388728da657ec7f8
jaime0delacruz0sonorasoft <jaime.delacruz@sonorasoft.com>
2023-04-16 14:44:11 -0700

    Como añadir texto al pdf

---
+++ Como crear un PDF con solo texto/CrearPDF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace CrearPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc = new Document();
+            PdfWriter.GetInstance(doc, new FileStream("archivo.pdf", FileMode.Create));
+            doc.Open();
+            doc.Add(new Paragraph("Hola mundo desde el código de Jaime"));
+            doc.Close();
+        }
+    }
+}
